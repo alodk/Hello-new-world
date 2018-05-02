0f18024ce980f005d92da8d083616ff5700b16cb
Anders Lund Olsen <38317079+alodk@users.noreply.github.com>
2018-05-02 05:31:08 +0200

    Testing functions
    Made a little program for making sine waves. Hopefully I can also make a triangle wave and a square wave. The goal is to morph between the three wave forms using numbers 0-127. 0=sine, 64=triangle, 127= square. These are the control codes from a Software synth modulation wheel or some other control nub/slider.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+#include <iostream>
+#include <windows.system.h>
+#include <string>
+int main() {
+	char a = 't';
+	int x = 19;
+	for (int n = 0; n < x; n++) {
+		cout << n << " Tonni har et krøllet skæg! " << a <<'\n';
+	}
+	cin >> a;
+	return 0;
+}
